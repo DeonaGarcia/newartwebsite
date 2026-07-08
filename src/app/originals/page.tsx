8fccd75f3264acfd8d00a8278219562b7c2d3529
DeonaGarcia <deonagarcia@yahoo.com>
2026-07-07 15:23:49 -1000

    Fix aspect ratio calculation for original art images

--- src/app/originals/page.tsx
+++ src/app/originals/page.tsx
@@ -35,7 +35,7 @@ export default async function OriginalsPage() {
               <div key={art.id} className="break-inside-avoid group">
                 <div
                   className="relative overflow-hidden bg-sand/30"
-                  style={{ aspectRatio: \`\${art.width}/\${art.height}\` }}
+                  style={{ aspectRatio: art.width + "/" + art.height }}
                 >
                   <Image
                     src={art.imageUrl}
